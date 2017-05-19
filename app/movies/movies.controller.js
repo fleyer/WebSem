8518b849d4dfa84146c260acf2628aa1c7e4dd5a
Clemsept <clement.toureille@free.fr>
2017-05-19 17:37:34 +0200

    Putain, ça y est !!!

--- app/movies/movies.controller.js
+++ app/movies/movies.controller.js
@@ -11,8 +11,67 @@
     angular.module('ng-starter')
         .controller('MoviesController', MoviesController);
 
-    MoviesController.$inject = ['$scope', '$state', '$timeout', '$log']; 
+    MoviesController.$inject = ['$scope', '$state', '$timeout', '$log','FuzekiService']; 
 
-    function MoviesController($scope, $state, $timeout, $log) {
+    function MoviesController($scope, $state, $timeout, $log, FuzekiService){
+		
+		var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+		var marker;
+		
+		FuzekiService.getCount().then(function(response){
+			var random = getRandomArbitrary(0,response.data.results.bindings[0].count.value-1);
+			var loc;
+			FuzekiService.getRandom(random).then(function(response2){
+				var map;
+				map = initMap();
+				
+				$scope.title = response2.data.results.bindings[0].title.value;
+				
+				response2.data.results.bindings.forEach(function(elem){
+					loc = showMarkerPlace(parseFloat(elem.latitude.value),parseFloat(elem.longitude.value),map);
+				});
+				
+				map.panTo(loc);
+			});
+		});
+		
+			/**
+		 * Returns a random number between min (inclusive) and max (exclusive)
+		 */
+		function getRandomArbitrary(min, max) {
+			return Math.round(Math.random() * (max - min) + min);
+		}
+
+		function initMap() {
+			
+			return new google.maps.Map(document.getElementById('map'), {
+				zoom:8,
+				center:{lat: 43.6, lng:1.4333},
+				mapTypeId: 'roadmap',
+				scrollwheel: false
+			});
+			
+		};
+		
+		function showMarkerPlace(latitude,longitude,p_map) {
+			
+			var loc;
+			var marker;
+			
+			loc = new google.maps.LatLng(latitude,longitude);
+			
+			marker = new google.maps.Marker({
+				position: loc,
+				map: p_map
+			});
+			
+			return loc;
+			
+		};
+	
     }
+	
+	
+	
 })();
--- app/movies/service/fuzeki.service.js
+++ app/movies/service/fuzeki.service.js
@@ -1,11 +1,68 @@
+(function(){
+	'use strict';
+	
+	angular
+		.module('ng-starter')
+		.service('FuzekiService', FuzekiService);
 
-angular
-	.module('ng-starter')
-    .service('FuzekiService', function FuzekiService(){
-		return {
-    		getData: function(){
+	FuzekiService.$inject = ['$http'];
 
-    		}
-    	}
+	function FuzekiService($http){
+		var apiFusekiServer = 'http://127.0.0.1:3030/cinema/query';
+		var prefixPayload = '\
+		PREFIX owl: <http://www.w3.org/2002/07/owl#> \
+		PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
+		prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+		prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
+		PREFIX : <http://www.semanticweb.org/clement/ontologies/2017/2/untitled-ontology-2#> ';
+		
+		return{
+			getCount: function(){
+				var payload = prefixPayload + '\
+				  SELECT (count(distinct ?film) as ?count) \
+					WHERE { \
+					  ?film a ?type. \
+					  ?type rdfs:label "Films"@fr. \
+					}';
 
-    });
+				return requestFusekiServer(payload);
+			},
+			
+			getRandom: function(os){
+				var payload = prefixPayload + 
+					"SELECT ?title ?latitude ?longitude "+
+					"WHERE { "+
+						"?film rdfs:label ?title. "+
+						"?film ?aPourAdresse ?adresse. "+
+						"?adresse ?aPourLatitude ?latitude. "+
+						"?adresse ?aPourLongitude ?longitude."+
+					  
+						"?aPourAdresse rdfs:label 'aPourAdresse'@fr. "+
+						"?aPourLongitude rdfs:label 'A pour longitude'@fr. "+
+						"?aPourLatitude rdfs:label 'A pour latitude'@fr. "+
+						"{"+
+							"Select ?film "+
+							"Where { "+
+								"?film a ?type. "+
+								"?type rdfs:label \"Films\"@fr. "+
+							"}"+
+							"order by ?film offset "+os+" Limit 1 "+
+						"}"+
+					"}";
+
+				return requestFusekiServer(payload);
+			}
+		};
+
+		function requestFusekiServer(payload){
+			var options ={
+				method: 'GET',
+				url: apiFusekiServer,
+				headers: {'Accept': 'application/sparql-results+json,*/*;q=0.9'},
+				params: {query: payload},// Will be urlencoded
+			};
+			
+			return $http(options);
+		}
+	};
+})();
